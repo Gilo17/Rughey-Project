69391c452838647f688b16c89e546edb71741e01
Rughey Gilzeane <gilzeane15@yahoo.com>
2022-12-10 13:28:02 -0500

    Authentication test

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://ui-automation-camp.vercel.app',
+    specPattern: 'cypress/e2e/test/*.cy.js',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    },
   },
 });
