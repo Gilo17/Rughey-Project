1f7050b42b6f67013cf30251a6a1e80d7e5221db
Rughey Gilzeane <gilzeane15@yahoo.com>
2022-12-13 12:07:13 -0500

    checkout

--- cypress/e2e/page/checkOut.page.js
+++ cypress/e2e/page/checkOut.page.js
@@ -1,54 +1,81 @@
 class Checkout {
 
-    get Checkoutbtn (){
-      return ("button[class='snipcart-button-primary snipcart-base-button is-icon-right']")  
+    get Checkoutbtn() {
+        return ("button[class='snipcart-button-primary snipcart-base-button is-icon-right']")
     }
 
-    get fullName () {
-        return ("#name_72f5ece5-bf87-4b76-a5ac-ceed54acda1e")     
+    get fullName() {
+        return ("#name_72f5ece5-bf87-4b76-a5ac-ceed54acda1e")
     }
 
-    get email () {
+    get email() {
         return ('#email_72f5ece5-bf87-4b76-a5ac-ceed54acda1e')
     }
 
-    get streetAdress () {
+    get streetAdress() {
         return ('#address1_72f5ece5-bf87-4b76-a5ac-ceed54acda1e')
     }
 
-    get city () {
+    get city() {
         return ('#city_72f5ece5-bf87-4b76-a5ac-ceed54acda1e')
     }
 
-    get country () {
+    get country() {
         return ('#country_72f5ece5-bf87-4b76-a5ac-ceed54acda1e')
     }
 
-    get state () {
+    get state() {
         return ('#province_72f5ece5-bf87-4b76-a5ac-ceed54acda1e')
     }
 
-    get zip () {
+    get zip() {
         return ('#postalCode_72f5ece5-bf87-4b76-a5ac-ceed54acda1e')
     }
 
-    get continueBtn () {
+    get continueBtn() {
         return ('.snipcart-base-button__wrapper')
     }
 
-    checkOut(fullName,email,streetAdress,city,country,state,zip){
+    get cardNumber() {
+        return ('[name = "card-number"]')
+    }
+    get cardDate() {
+        return ('[name = "expiry-date"]')
+    }
+    get cvvNumber() {
+        return ('#cvv')
+    }
+    get placeOrderButton() {
+        return ('[type="submit"]')
+    }
+
+    checkOutFlow(fullName, email, streetAdress, city, country, state, zip) {
         cy.get(this.Checkoutbtn).click()
         cy.get(this.fullName).type(fullName)
         cy.get(this.email).type(email)
         cy.get(this.streetAdress).type(streetAdress)
         cy.get(this.city).type(city)
-        cy.get(this.country).type(country)
+        cy.get(this.country).select(country)
         cy.get(this.state).type(state)
         cy.get(this.zip).type(zip)
-        cy.get(this.continueBtn).click()
+        cy.get(this.continueBtn).scrollIntoView()
+        cy.get(this.continueBtn.click({ force: true })
 
-    }  
-     
+
+
+        )
+    }
+
+    paymentInfo(cardNumber, date, cvv) {
+
+        cy.get(this.cardNumber).should('be.visible')
+
+        cy.get(this.cardNumber).type(cardNumber)
+        cy.get(this.cardDate).type(date)
+        cy.get(this.cvvNumber).type(cvv)
+        cy.get(this.placeOrderButton).click()
+    }
+
 }
 export default new Checkout()
 
