9fb5acc9ece17565ecb6c8ddf4441298593f4810
Rughey Gilzeane <gilzeane15@yahoo.com>
2022-12-14 20:27:36 -0500

    add to cart

--- cypress/e2e/test/addToCart.cy.js
+++ cypress/e2e/test/addToCart.cy.js
@@ -17,12 +17,30 @@ describe.only("Add to cart",()=>{
         cy.url().should('contain', 'products')
         cy.get(Authentication.productList).should('be.visible')
     })
-    it('add single item to cart',()=>{
+    it.skip('add single item to cart',()=>{
         cy.get('button[data-item-name="Quality Fitted Hat"]').scrollIntoView()
         cy.wait(6000)
         cy.get('button[data-item-name="Quality Fitted Hat"]').should('be.visible').click()
+        cy.get('.snipcart-base-button__wrapper').should("be.visible")
+    })
+    it.skip('remove single item from cart', ()=>{
+        cy.get('button[data-item-name="Quality Fitted Hat"]').scrollIntoView()
         cy.wait(6000)
-        cy.get(".snipcart-cart-header__title.snipcart__font--black.snipcart__font--secondary").should("be.visible")
+        cy.get('button[data-item-name="Quality Fitted Hat"]').should('be.visible').click()
+        cy.get('.snipcart-base-button__wrapper').should("be.visible")
+        cy.get("button[title='Decrement quantity']").click();
+        cy.url().should('contain','cart') 
+        cy.get('.snipcart-empty-cart__title.snipcart__font--secondary.snipcart__font--xlarge.snipcart__font--bold').should('be.visible')
+    })
+    it('should add multiple items to cart',()=>{
+        cy.get('button[data-item-name="Quality Fitted Hat"]').scrollIntoView()
+        cy.wait(6000)
+        cy.get('button[data-item-name="Quality Fitted Hat"]').should('be.visible').click()
+        cy.get('button[data-item-name="Quality Trucker Hat"]').scrollIntoView()
+        cy.wait(6000)
+        cy.get('button[data-item-name="Quality Trucker Hat"]').should('be.visible').click()
+        cy.get('.snipcart-base-button__wrapper').should("be.visible")
+        cy.get("button[title='Decrement quantity']").click(); 
     })
 
 
