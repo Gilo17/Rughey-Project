9c7b90133190534736694f5e595da31f7cb895c2
Rughey Gilzeane <gilzeane15@yahoo.com>
2022-12-15 14:53:24 -0500

    check out

--- cypress/e2e/test/checkOut.cy.js
+++ cypress/e2e/test/checkOut.cy.js
@@ -10,8 +10,8 @@ describe('CheckOut', () => {
             "https://dev-mlluudmotpwoldtv.us.auth0.com",
             { args: {} },
             ({ }) => {
-                cy.get('[type="email"]').type("test1@tester.com");
-                cy.get('[type="password"]').type("Password1", { log: false });
+                cy.get('[type="email"]').type("gilzene@yahoo.com");
+                cy.get('[type="password"]').type("Liverpool123", { log: false });
                 cy.get("[name=submit]").click();
             }
         );
@@ -47,7 +47,22 @@ describe('CheckOut', () => {
         cy.get('.snipcart__font--subtitle').should('be.visible')
 
     })
+    it('should not checkout with invalid card number', () => {
+        cy.get('button[data-item-name="Quality Fitted Hat"]').scrollIntoView()
+        cy.wait(6000)
+        cy.get('button[data-item-name="Quality Fitted Hat"]').should('be.visible').click()
+        cy.get(".snipcart-cart-header").should("be.visible")
+        cy.get('.snipcart-base-button__wrapper').should("be.visible")
+        Checkout.checkOutFlow('Rughey Gilzeane', 'gilzeane12@yahoo.com', 'Ivory Way', 'Mount Pleasant', 'Jamaica', 'St Andrew', '12345')
+        cy.iframe('.snipcart-payment-card-form iframe').find('#card-number').type('4242 4242 4242 4242')
+        cy.iframe('.snipcart-payment-card-form iframe').find('#expiry-date').type('10/24')
+        cy.iframe('.snipcart-payment-card-form iframe').find('#cvv').type(123)
+        cy.get('.snipcart-submit').click()
+        cy.url().should('contain', 'checkout')
+        cy.wait(6000)
+        cy.get('.snipcart__font--subtitle').should('be.visible')
 
+    })
 
 
 })
