6d19ffc0638f0e3a8f3089c6b4f20a2ecaeffc65
Rughey Gilzeane <gilzeane15@yahoo.com>
2022-12-16 18:43:48 -0500

    sort

---
+++ cypress/e2e/test/sort.cy.js
@@ -0,0 +1,30 @@
+import Authentication from "../page/authentication.page"
+import ProductsData from '../data/ProductsData.data'
+import addToCart from '../page/addToCart.page'
+describe('Sorting items names and prices', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+    it.skip('should sort products from A-Z', () => {
+        Authentication.login('gilzene@yahoo.com', 'Liverpool123')
+        addToCart.selectSort(ProductsData.sort['A to Z'])
+        ProductsData.products.sort()
+        
+    })
+    it.skip('should sort products from High to Low', () => {
+        Authentication.login('gilzene@yahoo.com', 'Liverpool123')
+        addToCart.selectSort(ProductsData.sort['High to Low'])
+        ProductsData.products.sort()
+        cy.get(addToCart.itemPrice).each(($elem, index) => {
+            expect($elem.text()).equal(ProductsData.products[index].price)
+        })
+    })
+    it('should filter products by hat category', () => {
+        Authentication.login('gilzene@yahoo.com', 'Liverpool123')
+        addToCart.selectFilter(ProductsData.sort['Hats'])
+        ProductsData.products.sort()
+        cy.get(addToCart.itemCategory).each(($elem, index) => {
+            expect($elem.text('hat')).equal(ProductsData.products[index].label)
+        })
+    })
+})
