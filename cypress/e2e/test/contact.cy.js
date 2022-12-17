1c3b473e280a90a24d0a48a51d4fb5f9ea384272
Rughey Gilzeane <gilzeane15@yahoo.com>
2022-12-17 08:55:44 -0500

    contact

--- cypress/e2e/test/contact.cy.js
+++ cypress/e2e/test/contact.cy.js
@@ -18,14 +18,18 @@ describe('CheckOut', () => {
         cy.url().should('contain', 'products')
         cy.get(Authentication.productList).should('be.visible')
     })
-    it.skip('should send a message',()=>{
+    it('should send a message',()=>{
         contact.Contact('Rughey','Gilzeane','gilzene@yahoo.com','clothing items','i would love for you to view these items')
+       cy.url().should('contain','contact')
     })
 
-    it.skip('should not send message with invalid email',()=>{
+    it('should not send message with invalid email',()=>{
         contact.Contact('Rughey','Gilzeane','gilzene@ya','clothing items','i would love for you to view these items')
+        cy.url().should('contain','contact')
+        cy.get("chakra-form__error-message css-170ki1a").should('contain.text','Email is invalid')
     })
     it('should not send message with numbers as name',()=>{
         contact.Contact('Rughey','123456','gilzene@yahoo.com','clothing items','i would love for you to view these items')
+        cy.url().should('contain','contact')
     })
 })
